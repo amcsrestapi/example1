f942cbb9ba16d26a873da719891aa1790e1d70f9
pod909 <pod909@gmail.com>
2021-08-20 17:01:49 +0100

    Creation of VS project

---
+++ AmcsRestApiExample1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AmcsRestApiExample1
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "AMCS REST API Example 1" );
+            Console.WriteLine( "Logging onto the API with a PAT token and retrieving a customer" );
+        }
+    }
+}
