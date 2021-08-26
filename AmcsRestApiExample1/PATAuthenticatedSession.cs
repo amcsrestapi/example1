c38a0cc1e7ce9effa45e9ad6a36cd0e5e55c477e
pod909 <pod909@gmail.com>
2021-08-26 17:57:19 +0100

    remove time out check

--- AmcsRestApiExample1/PATAuthenticatedSession.cs
+++ AmcsRestApiExample1/PATAuthenticatedSession.cs
@@ -10,45 +10,38 @@ namespace AmcsRestApiExample1.AuthenticatedSession
 {
     public class PATAuthenticatedSession : HttpClient
     {
-        private const int TimeOutMins = 30;
-        
         private readonly string PAT;
-        private DateTime AuthTimeout;
-
-        public bool IsAuthenticated => DateTime.Now < AuthTimeout;
 
         public PATAuthenticatedSession( Uri Domain, string PAT ) : base( new HttpClientHandler() { UseCookies = true } )
         {
             this.PAT = PAT;
-            this.AuthTimeout = DateTime.Now;
             BaseAddress = Domain;
-            Authenticate();
         }
 
-        public async void Authenticate()
+        public async Task<bool> Authentication()
         {
-            HttpResponseMessage Response = await SendAsync( 
-                new HttpRequestMessage( HttpMethod.Post, BaseAddress ) {
+            var Response = await AuthenticationResponse();
+            if( Response.StatusCode != HttpStatusCode.OK )
+                throw new AuthenticationException();
+            return true;
+        }
+
+        public async Task<HttpResponseMessage> AuthenticationResponse()
+            => await SendAsync(
+                new HttpRequestMessage( HttpMethod.Post, BaseAddress )
+                {
                     Content = new StringContent(
                         "{ 'privatekey':'"+PAT+ "' }", Encoding.UTF8, "application/json"
                     )
                 }
             );
 
-            if( Response.StatusCode != HttpStatusCode.OK )
-                throw new AuthenticationException();
-
-            AuthTimeout = DateTime.Now.AddMinutes( TimeOutMins );
-        }
-
         public override async Task<HttpResponseMessage> SendAsync( HttpRequestMessage request, CancellationToken cancellationToken )
         {
-            if( !IsAuthenticated )
-                Authenticate();
             HttpResponseMessage Response = await base.SendAsync( request, cancellationToken );
             if( Response.StatusCode == HttpStatusCode.Unauthorized )
             {
-                Authenticate();
+                await Authentication();
                 Response = await base.SendAsync( request, cancellationToken );
             }
             return Response;
